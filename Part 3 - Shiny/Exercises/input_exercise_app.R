c6ee6bdf01f142d3a9cb4d53d70e8a740aa8e44d
Dizyd <David.Izydorczyk@gmail.com>
2020-05-12 16:07:26 +0200

    fixed error input_exercise fill -> color

--- Part 3 - Shiny/Exercises/input_exercise_app.R
+++ Part 3 - Shiny/Exercises/input_exercise_app.R
@@ -82,7 +82,7 @@ server <- function(input, output) {
         p <- ggplot(phd_top_n, aes(x = factor(year), y = log(n_phds), color = group, group = group)) +
           geom_line() +
           geom_point() +
-          scale_fill_brewer(palette=input$palette) +
+          scale_color_brewer(palette=input$palette) +
           labs(fill = groupBY,
                x     = "Year",
                y     = "Nr. of PhDs",
